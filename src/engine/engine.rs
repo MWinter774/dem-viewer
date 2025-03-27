f8f223c56f5085b7fca85723aefee291f08f0044
Maor Winter <winter.maor@gmail.com>
2025-03-27 08:54:06 +0200

    Basic window creating

--- src/engine/engine.rs
+++ src/engine/engine.rs
@@ -1,8 +1,14 @@
-pub struct Engine {}
+use crate::engine;
+
+pub struct Engine {
+    context: engine::EngineContext,
+}
 
 impl Engine {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            context: engine::EngineContext::new("DEM Viewer", 800, 600),
+        }
     }
 
     pub fn run(&mut self) {}
--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -1,3 +1,11 @@
 pub use engine::Engine;
+use engine_context::EngineContext;
+use glfw_context::GLFWContext;
+use window::Window;
+use input_system::InputSystem;
 
 mod engine;
+mod engine_context;
+mod glfw_context;
+mod window;
+mod input_system;
