be6f600ff6c4fe6d2b0e203df02e7814a6b0041e
Maor Winter <winter.maor@gmail.com>
2025-03-28 22:51:24 +0300

    Generating vertices and indices for geotiff files

--- src/engine/models/terrain.rs
+++ src/engine/models/terrain.rs
@@ -2,12 +2,16 @@ use crate::engine::models;
 
 pub struct Terrain {
     terrain_data: models::TerrainData,
+    terrain_render_data: models::TerrainRenderData,
 }
 
 impl Terrain {
     pub fn from_geotiff_file(geotiff_file_path: &str) -> Self {
+        let terrain_data = models::TerrainData::from_geotiff_file(geotiff_file_path);
+        let terrain_render_data = models::TerrainRenderData::new(&terrain_data);
         Self {
-            terrain_data: models::TerrainData::from_geotiff_file(geotiff_file_path),
+            terrain_data,
+            terrain_render_data,
         }
     }
 }
