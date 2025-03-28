a384d0bf27eb54bea92429458a2dfeab0ee6b0d4
Maor Winter <winter.maor@gmail.com>
2025-03-28 21:42:26 +0300

    Camera integrated in engine

--- src/engine/engine.rs
+++ src/engine/engine.rs
@@ -2,14 +2,47 @@ use crate::engine;
 
 pub struct Engine {
     context: engine::EngineContext,
+    camera: engine::Camera,
 }
 
 impl Engine {
     pub fn new() -> Self {
         Self {
             context: engine::EngineContext::new("DEM Viewer", 800, 600),
+            camera: engine::Camera::default(),
         }
     }
 
-    pub fn run(&mut self) {}
+    pub fn run(&mut self) {
+        let mut window_should_close = false;
+
+        self.context.initialize();
+
+        unsafe {
+            gl::Enable(gl::DEPTH_TEST);
+            gl::DepthFunc(gl::LESS);
+        }
+
+        while !window_should_close {
+            // Swap the buffers of the opengl window and updates event pipeline
+            let frame_data = self.context.next_frame();
+            self.camera
+                .update_input(&frame_data.input_system, frame_data.delta_time as f32);
+
+            unsafe {
+                gl::Clear(gl::DEPTH_BUFFER_BIT | gl::COLOR_BUFFER_BIT);
+                gl::ClearColor(0.0, 0.0, 0.5, 0.0);
+            }
+
+            if frame_data
+                .input_system
+                .keyboard
+                .get_key_press_state(glfw::Key::Escape)
+                == glfw::Action::Press
+            {
+                window_should_close = true;
+            }
+        }
+        self.context.set_should_terminate(true);
+    }
 }
--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -3,9 +3,12 @@ use engine_context::EngineContext;
 use glfw_context::GLFWContext;
 use window::Window;
 use input_system::InputSystem;
+use camera::Camera;
 
 mod engine;
 mod engine_context;
 mod glfw_context;
 mod window;
 mod input_system;
+mod camera;
+mod transformations;
