566d92e4d9b2381018532d7c324db56aab87e923
Maor Winter <winter.maor@gmail.com>
2025-03-28 23:46:09 +0300

    Made terrain_render_data and terrain_data public structures

--- src/engine/models/mod.rs
+++ src/engine/models/mod.rs
@@ -1,6 +1,6 @@
 pub use terrain::Terrain;
-use terrain_data::TerrainData;
-use terrain_render_data::TerrainRenderData;
+pub use terrain_data::TerrainData;
+pub use terrain_render_data::TerrainRenderData;
 
 mod terrain;
 mod terrain_data;
