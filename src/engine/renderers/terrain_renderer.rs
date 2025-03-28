32e0ea0a8373b740b00317a85910c94778a57c2f
Maor Winter <winter.maor@gmail.com>
2025-03-28 23:47:49 +0300

    Basic work on terrain_renderer

--- src/engine/renderers/terrain_renderer.rs
+++ src/engine/renderers/terrain_renderer.rs
@@ -1,3 +1,16 @@
-pub struct TerrainRenderer {}
+use crate::engine::models;
+use crate::engine::shader_programs;
 
-impl TerrainRenderer {}
+pub struct TerrainRenderer<'a> {
+    terrain_shader_program: shader_programs::TerrainShaderProgram<'a>,
+}
+
+impl<'a> TerrainRenderer<'a> {
+    pub fn new(terrain_render_data: &'a models::TerrainRenderData) -> Self {
+        let terrain_shader_program =
+            shader_programs::TerrainShaderProgram::new(terrain_render_data.get_vertices());
+        Self {
+            terrain_shader_program,
+        }
+    }
+}
