1eda47096f24ee136d0b700c52874e82642e9722
Maor Winter <winter.maor@gmail.com>
2025-03-29 09:39:42 +0300

    Terrain now has an OpenGL object

--- src/engine/models/terrain_opengl_object.rs
+++ src/engine/models/terrain_opengl_object.rs
@@ -1,12 +1,31 @@
-use crate::engine::opengl;
+use crate::engine::{models, opengl};
+
+const VERTICES_VBO_INDEX: usize = 0;
+const INDICES_VBO_INDEX: usize = 1;
 
 pub struct TerrainOpenGLObject {
-    terrain_vao: opengl::VAO,
+    terrain_vao: opengl::VAO2Buffer,
 }
 
 impl TerrainOpenGLObject {
-    pub fn new() -> Self {
-        let terrain_vao = opengl::VAO::new();
+    pub fn new(terrain_render_data: &models::TerrainRenderData) -> Self {
+        let terrain_vao = opengl::VAO2Buffer::new();
+        Self::load_terrain_render_data_to_terrain_vao(&terrain_vao, terrain_render_data);
         Self { terrain_vao }
+    }
+
+    fn load_terrain_render_data_to_terrain_vao(
+        terrain_vao: &opengl::VAO2Buffer,
+        terrain_render_data: &models::TerrainRenderData,
+    ) {
+        terrain_vao.bind_vertex_array();
+
+        // Loads vertices of terrain to vbo object
+        terrain_vao.bind_vbo_as_array_buffer(VERTICES_VBO_INDEX);
+        terrain_vao.load_array_buffer(VERTICES_VBO_INDEX, terrain_render_data.get_vertices());
+
+        // Loads indices of terrain to vbo object
+        terrain_vao.bind_vbo_as_element_array_buffer(INDICES_VBO_INDEX);
+        terrain_vao.load_element_array_buffer(INDICES_VBO_INDEX, terrain_render_data.get_indices());
     }
 }
