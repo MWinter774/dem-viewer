8c23d565fd18a0a64c95b435cd2602777d67ae0e
Maor Winter <winter.maor@gmail.com>
2025-03-29 17:37:27 +0300

    Renamed TerrainData to TerrainModelData

--- src/engine/models/mod.rs
+++ src/engine/models/mod.rs
@@ -1,9 +1,9 @@
 pub use terrain::Terrain;
-pub use terrain_data::TerrainData;
+pub use terrain_model_data::TerrainModelData;
 pub use terrain_opengl_object::TerrainOpenGLObject;
 pub use terrain_render_data::TerrainRenderData;
 
 mod terrain;
-mod terrain_data;
+mod terrain_model_data;
 mod terrain_opengl_object;
 mod terrain_render_data;
