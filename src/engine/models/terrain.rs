c64d63e5c1749ece2611c87e900f89ecf91845b2
Maor Winter <winter.maor@gmail.com>
2025-03-29 17:43:02 +0300

    Added object to hold position data of models

--- src/engine/models/terrain.rs
+++ src/engine/models/terrain.rs
@@ -4,6 +4,7 @@ pub struct Terrain {
     terrain_data: models::TerrainModelData,
     terrain_render_data: models::TerrainRenderData,
     terrain_opengl_object: models::TerrainOpenGLObject,
+    terrain_model_position_data: models::ModelPositionData,
 }
 
 impl Terrain {
@@ -11,10 +12,12 @@ impl Terrain {
         let terrain_data = models::TerrainModelData::from_geotiff_file(geotiff_file_path);
         let terrain_render_data = models::TerrainRenderData::new(&terrain_data);
         let terrain_opengl_object = models::TerrainOpenGLObject::new(&terrain_render_data);
+        let terrain_model_position_data = models::ModelPositionData::default();
         Self {
             terrain_data,
             terrain_render_data,
             terrain_opengl_object,
+            terrain_model_position_data
         }
     }
 
