c030e52aae893fc92ba841e119b86914422c0a41
Maor Winter <winter.maor@gmail.com>
2025-03-29 09:18:19 +0300

    Created object to hold opengl objects of terrain

--- src/engine/models/mod.rs
+++ src/engine/models/mod.rs
@@ -1,7 +1,9 @@
 pub use terrain::Terrain;
 pub use terrain_data::TerrainData;
+use terrain_opengl_object::TerrainOpenGLObject;
 pub use terrain_render_data::TerrainRenderData;
 
 mod terrain;
 mod terrain_data;
+mod terrain_opengl_object;
 mod terrain_render_data;
