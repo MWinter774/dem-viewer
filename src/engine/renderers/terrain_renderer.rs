418a06fb2d65de99005d8055eed1b197bf83b079
Maor Winter <winter.maor@gmail.com>
2025-03-29 09:55:58 +0300

    Terrain can now be rendererd

--- src/engine/renderers/terrain_renderer.rs
+++ src/engine/renderers/terrain_renderer.rs
@@ -1,5 +1,5 @@
-use crate::engine::models;
-use crate::engine::shader_programs;
+use crate::engine::{models, shader_programs};
+use nalgebra_glm as glm;
 
 pub struct TerrainRenderer<'a> {
     terrain_shader_program: shader_programs::TerrainShaderProgram<'a>,
@@ -11,6 +11,23 @@ impl<'a> TerrainRenderer<'a> {
             shader_programs::TerrainShaderProgram::new(terrain_render_data.get_vertices());
         Self {
             terrain_shader_program,
+        }
+    }
+
+    pub fn render_terrain(&mut self, terrain: models::Terrain, mvp_matrix: &glm::Mat4) {
+        self.terrain_shader_program.use_program();
+        terrain.get_terrain_opengl_object().bind_vao();
+        self.terrain_shader_program.enable_vertex_attrib_array();
+        self.terrain_shader_program
+            .set_mvp_uniform_variable(mvp_matrix);
+
+        unsafe {
+            gl::DrawElements(
+                gl::TRIANGLES,
+                terrain.get_terrain_render_data().get_indices().len() as i32,
+                gl::UNSIGNED_INT,
+                std::ptr::null(),
+            );
         }
     }
 }
