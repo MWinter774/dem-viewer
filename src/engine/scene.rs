82085f127a69c077b7f2f4a71694f11ae6a6cedc
Maor Winter <winter.maor@gmail.com>
2025-03-29 10:49:54 +0300

    More work on scene object

--- src/engine/scene.rs
+++ src/engine/scene.rs
@@ -1,13 +1,17 @@
-use super::models;
+use crate::engine::{models, renderers};
 
 pub struct Scene {
     terrain: models::Terrain,
+    terrain_renderer: renderers::TerrainRenderer,
 }
 
 impl Scene {
     pub fn new(geotiff_file_path: &str) -> Self {
+        let terrain = models::Terrain::from_geotiff_file(geotiff_file_path);
+        let terrain_renderer = renderers::TerrainRenderer::new();
         Self {
-            terrain: models::Terrain::from_geotiff_file(geotiff_file_path),
+            terrain,
+            terrain_renderer,
         }
     }
 }
