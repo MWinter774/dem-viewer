2536e394cb4b020610d3d60370c438a72a8859e0
Maor Winter <winter.maor@gmail.com>
2025-03-29 19:10:45 +0300

    Terrain opengl object now holds uv coords

--- src/engine/models/terrain_opengl_object.rs
+++ src/engine/models/terrain_opengl_object.rs
@@ -2,14 +2,15 @@ use crate::engine::{models, opengl};
 
 const VERTICES_VBO_INDEX: usize = 0;
 const INDICES_VBO_INDEX: usize = 1;
+const UV_VBO_INDEX: usize = 2;
 
 pub struct TerrainOpenGLObject {
-    terrain_vao: opengl::VAO2Buffer,
+    terrain_vao: opengl::VAO3Buffer,
 }
 
 impl TerrainOpenGLObject {
     pub fn new(terrain_render_data: &models::TerrainRenderData) -> Self {
-        let terrain_vao = opengl::VAO2Buffer::new();
+        let terrain_vao = opengl::VAO3Buffer::new();
         Self::load_terrain_render_data_to_terrain_vao(&terrain_vao, terrain_render_data);
         Self { terrain_vao }
     }
@@ -19,7 +20,7 @@ impl TerrainOpenGLObject {
     }
 
     fn load_terrain_render_data_to_terrain_vao(
-        terrain_vao: &opengl::VAO2Buffer,
+        terrain_vao: &opengl::VAO3Buffer,
         terrain_render_data: &models::TerrainRenderData,
     ) {
         terrain_vao.bind_vertex_array();
@@ -31,5 +32,9 @@ impl TerrainOpenGLObject {
         // Loads indices of terrain to vbo object
         terrain_vao.bind_vbo_as_element_array_buffer(INDICES_VBO_INDEX);
         terrain_vao.load_element_array_buffer(INDICES_VBO_INDEX, terrain_render_data.get_indices());
+
+        // Loads uv texture coords of terrain to vbo object
+        terrain_vao.bind_vbo_as_element_array_buffer(UV_VBO_INDEX);
+        terrain_vao.load_array_buffer(UV_VBO_INDEX, terrain_render_data.get_uv());
     }
 }
