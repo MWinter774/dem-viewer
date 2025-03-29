ea75c825b10371fd096fbea045dffd88aa861650
Maor Winter <winter.maor@gmail.com>
2025-03-29 17:43:02 +0300

    Added object to hold position data of models

--- src/engine/models/mod.rs
+++ src/engine/models/mod.rs
@@ -1,8 +1,10 @@
+pub use model_position_data::ModelPositionData;
 pub use terrain::Terrain;
 pub use terrain_model_data::TerrainModelData;
 pub use terrain_opengl_object::TerrainOpenGLObject;
 pub use terrain_render_data::TerrainRenderData;
 
+mod model_position_data;
 mod terrain;
 mod terrain_model_data;
 mod terrain_opengl_object;
