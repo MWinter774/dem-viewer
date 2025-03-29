a56d448c562e99818153a2ba5e6502552bdd4322
Maor Winter <winter.maor@gmail.com>
2025-03-29 21:39:39 +0300

    Provide texture

--- src/engine/engine.rs
+++ src/engine/engine.rs
@@ -23,7 +23,7 @@ impl Engine {
             gl::DepthFunc(gl::LESS);
         }
 
-        let mut scene = engine::Scene::new("DEM\\1.tif");
+        let mut scene = engine::Scene::new("DEM\\1.tif", "textures\\grass.jpg");
 
         while !window_should_close {
             // Swap the buffers of the opengl window and updates event pipeline
