39fbc2e84220eeb65ceae76e0102212a31e265c8
Maor Winter <winter.maor@gmail.com>
2025-03-29 09:18:19 +0300

    Created object to hold opengl objects of terrain

--- src/engine/models/terrain.rs
+++ src/engine/models/terrain.rs
@@ -3,15 +3,18 @@ use crate::engine::models;
 pub struct Terrain {
     terrain_data: models::TerrainData,
     terrain_render_data: models::TerrainRenderData,
+    terrain_opengl_object: models::TerrainOpenGLObject,
 }
 
 impl Terrain {
     pub fn from_geotiff_file(geotiff_file_path: &str) -> Self {
         let terrain_data = models::TerrainData::from_geotiff_file(geotiff_file_path);
         let terrain_render_data = models::TerrainRenderData::new(&terrain_data);
+        let terrain_opengl_object = models::TerrainOpenGLObject::new();
         Self {
             terrain_data,
             terrain_render_data,
+            terrain_opengl_object,
         }
     }
 }
