b59d79da9f40e0226c2a568819f093894b42af96
Maor Winter <winter.maor@gmail.com>
2025-03-29 17:43:02 +0300

    Added object to hold position data of models

---
+++ src/engine/models/model_position_data.rs
@@ -0,0 +1,40 @@
+use crate::engine::transformations;
+use nalgebra_glm as glm;
+
+pub struct ModelPositionData {
+    translation_matrix: transformations::Translation,
+    rotation_matrix: transformations::Rotation,
+    scaling_matrix: transformations::Scaling,
+}
+
+impl ModelPositionData {
+    pub fn new(
+        translation_matrix: transformations::Translation,
+        rotation_matrix: transformations::Rotation,
+        scaling_matrix: transformations::Scaling,
+    ) -> Self {
+        let translation_matrix = transformations::Translation::default();
+        let rotation_matrix = transformations::Rotation::default();
+        let scaling_matrix = transformations::Scaling::default();
+        Self {
+            translation_matrix,
+            rotation_matrix,
+            scaling_matrix,
+        }
+    }
+
+    pub fn get_model_matrix(&self) -> glm::Mat4 {
+        self.translation_matrix.get_matrix()
+            * self.rotation_matrix.get_matrix()
+            * self.scaling_matrix.get_matrix()
+    }
+}
+
+impl Default for ModelPositionData {
+    fn default() -> Self {
+        let translation_matrix = transformations::Translation::default();
+        let rotation_matrix = transformations::Rotation::default();
+        let scaling_matrix = transformations::Scaling::default();
+        ModelPositionData::new(translation_matrix, rotation_matrix, scaling_matrix)
+    }
+}
