ffbb6ab964e140a695d0f75ec142334643540e54
Maor Winter <winter.maor@gmail.com>
2025-03-29 19:38:04 +0300

    Support for uv coords in shader

--- src/engine/shader_programs/terrain_shader_program.rs
+++ src/engine/shader_programs/terrain_shader_program.rs
@@ -5,6 +5,7 @@ use crate::engine::opengl;
 pub struct TerrainShaderProgram {
     shader_program: opengl::ShaderProgram,
     vertex_attrib_pointer: opengl::VertexAttributePointer,
+    uv_vertex_attrib_pointer: opengl::VertexAttributePointer,
     mvp_uniform_variable: std::rc::Rc<opengl::UniformVariable>,
 }
 
@@ -14,11 +15,16 @@ impl TerrainShaderProgram {
             opengl::ShaderProgram::new("shaders\\terrain_shader.vs", "shaders\\terrain_shader.fs");
         let vertex_attrib_pointer =
             opengl::VertexAttributePointer::new(opengl::VertexAttributePointerConfig::default());
+        let mut uv_vertex_attrib_pointer_config = opengl::VertexAttributePointerConfig::default();
+        uv_vertex_attrib_pointer_config.index = 1;
+        uv_vertex_attrib_pointer_config.size = 2;
+        let uv_vertex_attrib_pointer = opengl::VertexAttributePointer::new(uv_vertex_attrib_pointer_config);
         let mvp_uniform_variable =
             std::rc::Rc::clone(&shader_program.get_uniform_variable("MVP").unwrap());
         Self {
             shader_program,
             vertex_attrib_pointer,
+            uv_vertex_attrib_pointer,
             mvp_uniform_variable,
         }
     }
@@ -29,6 +35,9 @@ impl TerrainShaderProgram {
 
     pub fn enable_vertex_attrib_array(&self) {
         self.vertex_attrib_pointer.enable_vertex_attrib_array();
+    }
+    pub fn enable_uv_attrib_array(&self) {
+        self.uv_vertex_attrib_pointer.enable_vertex_attrib_array();
     }
 
     pub fn set_mvp_uniform_variable(&mut self, mvp_matrix: &glm::Mat4) {
