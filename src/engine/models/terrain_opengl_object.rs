4ca3422441657d0ed67847040746b01fe6bd83a2
Maor Winter <winter.maor@gmail.com>
2025-03-29 09:22:22 +0300

    Started work on terrain opengl object

--- src/engine/models/terrain_opengl_object.rs
+++ src/engine/models/terrain_opengl_object.rs
@@ -1,7 +1,12 @@
-pub struct TerrainOpenGLObject {}
+use crate::engine::opengl;
+
+pub struct TerrainOpenGLObject {
+    terrain_vao: opengl::VAO,
+}
 
 impl TerrainOpenGLObject {
     pub fn new() -> Self {
-        Self {}
+        let terrain_vao = opengl::VAO::new();
+        Self { terrain_vao }
     }
 }
