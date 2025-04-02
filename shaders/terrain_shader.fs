d76691c13fa2a410f12ee7aa562297a83e1985cf
Maor Winter <winter.maor@gmail.com>
2025-04-02 22:17:24 +0300

    Fragment shader fix

--- shaders/terrain_shader.fs
+++ shaders/terrain_shader.fs
@@ -8,6 +8,6 @@ out vec4 FragColor;
 
 void main()
 {
-    float grayscale = clamp(1 - heightValue * 0.0004, 0.0, FragColor.y / FragColor.y);
+    float grayscale = clamp(1 - heightValue * 0.0004, 0.0, 1.0);
     FragColor = vec4(vec3(grayscale), 1.0);
 }
