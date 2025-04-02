a11791d8c70ac2741c1d03743c7bb3e313fb06e1
Maor Winter <winter.maor@gmail.com>
2025-04-02 22:19:00 +0300

    Fragment shader smoother height change

--- shaders/terrain_shader.fs
+++ shaders/terrain_shader.fs
@@ -9,5 +9,5 @@ out vec4 FragColor;
 void main()
 {
     float grayscale = clamp(1 - heightValue * 0.0004, 0.0, 1.0);
-    FragColor = vec4(vec3(grayscale), 1.0);
+    FragColor = vec4(vec3(sin(grayscale)), 1.0);
 }
