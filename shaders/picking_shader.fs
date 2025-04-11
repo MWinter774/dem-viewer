01e2ecd8effb0958f070b4bbb7b4e0eb48d817bf
Maor Winter <winter.maor@gmail.com>
2025-04-11 15:01:43 +0300

    Attaching relevant data to pixel

--- shaders/picking_shader.fs
+++ shaders/picking_shader.fs
@@ -8,6 +8,5 @@ void main()
     int i = int(vid);
     int r = (i & 0x000000FF) >>  0;
     int g = (i & 0x0000FF00) >>  8;
-    int b = (i & 0x00FF0000) >> 16;
-    FragColor = uvec3(r, g, b);
+    FragColor = uvec3(r, g, gl_PrimitiveID);
 }
