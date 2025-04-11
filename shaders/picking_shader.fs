3bb8a474ce03042d4727c07918f1b0caa490dfc8
Maor Winter <winter.maor@gmail.com>
2025-04-11 13:30:13 +0300

    Picking fragment shader outputs uvec3

--- shaders/picking_shader.fs
+++ shaders/picking_shader.fs
@@ -1,13 +1,9 @@
 #version 330 core
 flat in uint vid;
 
-out vec4 FragColor;
+out uvec3 FragColor;
 
 void main()
 {
-    int i = int(vid);
-    int r = (i & 0x000000FF) >>  0;
-    int g = (i & 0x0000FF00) >>  8;
-    int b = (i & 0x00FF0000) >> 16;
-    FragColor = vec4(vec3(r / 255.0, g / 255.0, b / 255.0), 1.0);
+    FragColor = uvec3(vid, vid, vid);
 }
