e430391c05a292fd0156272c51a8523f7c1d69aa
Maor Winter <winter.maor@gmail.com>
2025-04-12 12:10:51 +0300

    Using new dynamic array buffer

--- src/engine/models/highlight_opengl_object.rs
+++ src/engine/models/highlight_opengl_object.rs
@@ -1,7 +1,14 @@
-pub struct HighlightOpenGLObject {}
+use crate::engine::opengl;
+
+pub struct HighlightOpenGLObject {
+    highlight_vao: opengl::VAO1Buffer,
+}
 
 impl HighlightOpenGLObject {
     pub fn new() -> Self {
-        Self {}
+        let highlight_vao = opengl::VAO1Buffer::new();
+        // Tells OpenGL to allocate enough space for 3 vertices of the triangle that will be highlighted
+        highlight_vao.load_dynamic_array_buffer(3 * std::mem::size_of::<f32>() as isize);
+        Self { highlight_vao }
     }
 }
