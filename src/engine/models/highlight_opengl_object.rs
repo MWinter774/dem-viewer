a7cf350eed0ba29deb0b236c45297ef9da03bf80
Maor Winter <winter.maor@gmail.com>
2025-04-12 12:20:26 +0300

    Rebasing

--- src/engine/models/highlight_opengl_object.rs
+++ src/engine/models/highlight_opengl_object.rs
@@ -8,7 +8,7 @@ impl HighlightOpenGLObject {
     pub fn new() -> Self {
         let highlight_vao = opengl::VAO1Buffer::new();
         // Tells OpenGL to allocate enough space for 3 vertices of the triangle that will be highlighted
-        highlight_vao.load_dynamic_array_buffer(3 * std::mem::size_of::<f32>() as isize);
+        highlight_vao.init_dynamic_array_buffer(3 * std::mem::size_of::<f32>() as isize);
         Self { highlight_vao }
     }
 }
