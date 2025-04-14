450ffeaf3dc775abb25d03f6beb42030ccc9ee77
Maor Winter <winter.maor@gmail.com>
2025-04-14 13:43:37 +0300

    Drawing points on picture

--- src/engine/opencv_window.rs
+++ src/engine/opencv_window.rs
@@ -1,17 +1,42 @@
-use std::str::FromStr;
+use std::{cell, rc, str::FromStr, sync};
 
-use opencv::{highgui, prelude::*};
+use opencv::{self, core, highgui, prelude::*};
+
+const ESCAPE_KEY: i32 = 27;
 
 pub struct OpenCVWindow {
     window_title: String,
+    points: sync::Arc<sync::Mutex<Vec<core::Point>>>,
 }
 
 impl OpenCVWindow {
     pub fn new(window_title: String) -> Self {
-        Self { window_title }
+        Self {
+            window_title,
+            points: sync::Arc::new(sync::Mutex::new(Vec::new())),
+        }
     }
 
-    pub fn capture_points(&self, mut pixels: Vec<u8>, window_height: usize) {
+    pub fn capture_points(&self, pixels: Vec<u8>, window_height: usize) {
+        let mut image = self.pixels_to_image(pixels, window_height);
+
+        highgui::named_window(&self.window_title, highgui::WINDOW_AUTOSIZE).unwrap();
+        self.register_mouse_callback();
+
+        loop {
+            self.draw_points(&mut image);
+            highgui::imshow(&self.window_title, &image).unwrap();
+            
+            let key = highgui::wait_key(20).unwrap();
+            if key == ESCAPE_KEY {
+                break;
+            }
+        }
+
+        highgui::destroy_window(&self.window_title).unwrap();
+    }
+
+    fn pixels_to_image(&self, mut pixels: Vec<u8>, window_height: usize) -> Mat {
         // Convert RGB to BGR (OpenCV expects BGR)
         for chunk in pixels.chunks_exact_mut(3) {
             chunk.swap(0, 2); // swap R and B
@@ -22,19 +47,39 @@ impl OpenCVWindow {
         let mut flipped = Mat::default();
         opencv::core::flip(&mat, &mut flipped, 0).unwrap();
 
-        highgui::imshow(&self.window_title, &flipped).unwrap();
+        flipped
+    }
+
+    fn register_mouse_callback(&self) {
+        let points = self.points.clone();
         highgui::set_mouse_callback(
             &self.window_title,
             Some(Box::new(move |event, x, y, _flags| {
-                Self::mouse_callback(event, x, y, _flags);
+                Self::mouse_callback(event, x, y, _flags, &mut points.lock().unwrap());
             })),
         )
         .unwrap();
-        highgui::wait_key(0).unwrap();
     }
 
-    fn mouse_callback(event: i32, x: i32, y: i32, _flags: i32) {
-        if event == highgui::EVENT_LBUTTONDOWN {}
+    fn draw_points(&self, display_img: &mut Mat) {
+        for point in self.points.lock().unwrap().iter() {
+            opencv::imgproc::circle(
+                display_img,
+                *point,
+                5,
+                opencv::core::Scalar::new(0.0, 0.0, 255.0, 0.0),
+                -1,
+                opencv::imgproc::LINE_8,
+                0,
+            )
+            .unwrap();
+        }
+    }
+
+    fn mouse_callback(event: i32, x: i32, y: i32, _flags: i32, points: &mut Vec<core::Point>) {
+        if event == highgui::EVENT_LBUTTONDOWN {
+            points.push(core::Point::new(x, y));
+        }
     }
 }
 
