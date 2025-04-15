1ffc01652b653e5e691282e9a8a693ddb1eceb46
Maor Winter <winter.maor@gmail.com>
2025-04-15 22:00:24 +0300

    Rendering picked points

--- src/engine/camera_view/mod.rs
+++ src/engine/camera_view/mod.rs
@@ -1,7 +1,5 @@
 pub use camera_view_app::CameraViewApp;
-pub use camera_view_point::CameraViewPoint;
 pub use camera_view_window::CameraViewWindow;
 
 mod camera_view_app;
-mod camera_view_point;
 mod camera_view_window;
