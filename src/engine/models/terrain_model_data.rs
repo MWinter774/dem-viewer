6110ee94e03d4f182f2f6cd69616088a2954b48b
Maor Winter <winter.maor@gmail.com>
2025-06-24 13:20:16 +0300

    Removed outdated library related to geotiff loading

--- src/engine/models/terrain_model_data.rs
+++ src/engine/models/terrain_model_data.rs
@@ -1,46 +1,38 @@
+extern crate tiff;
+use std::fs::File;
+use tiff::decoder::{Decoder, DecodingResult};
+
 pub struct TerrainModelData {
-    ds: gdal_win::Dataset,
     width: usize,
     height: usize,
-    data: Vec<f64>,
+    data: Vec<f32>,
 }
 
 impl TerrainModelData {
     pub fn from_geotiff_file(geotiff_file_path: &str) -> Self {
-        let ds = gdal_win::Dataset::open(geotiff_file_path).unwrap();
-        let rasterband = ds.rasterband(1).unwrap();
-        let (width, height) = rasterband.size();
+        let mut decoder = Decoder::new(File::open(geotiff_file_path).unwrap())
+            .expect("Cannot create GeoTIFF decoder");
+        decoder = decoder.with_limits(tiff::decoder::Limits::unlimited());
+        let (width, height) = decoder.dimensions().unwrap();
+        let (width, height) = (width as usize, height as usize);
         let mut data = vec![0.0; width * height];
-        rasterband
-            .read_into_slice(
-                (0, 0),
-                rasterband.size(),
-                (width, height),
-                data.as_mut_slice(),
-                None,
-            )
-            .unwrap();
+        if let DecodingResult::F32(img) = decoder.read_image().unwrap() {
+            data = img;
+        } else {
+            panic!("Wrong data type");
+        }
         Self {
-            ds,
             width,
             height,
             data,
         }
     }
 
-    pub fn get_rasterband(&self) -> gdal_win::raster::RasterBand<'_> {
-        self.ds.rasterband(1).unwrap()
-    }
-
     pub fn get_size(&self) -> (usize, usize) {
         (self.width, self.height)
     }
 
-    pub fn get_geo_transform(&self) -> [f64; 6] {
-        self.ds.geo_transform().unwrap()
-    }
-
-    pub fn get_data(&self) -> &Vec<f64> {
+    pub fn get_data(&self) -> &Vec<f32> {
         &self.data
     }
 }
