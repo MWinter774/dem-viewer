33e3dd73f8dec2019c954ffc06955f0b0357128d
Maor Winter <winter.maor@gmail.com>
2025-09-03 17:09:12 +0300

    Fixed JSON format

--- src/engine/scene_initializer_for_presentation.rs
+++ src/engine/scene_initializer_for_presentation.rs
@@ -37,30 +37,31 @@ impl SceneInitializerForPresentation {
         picked_points: &Vec<engine::epnp::EPnPPicturePoint>,
     ) -> String {
         let mut s = String::new();
-        s.push_str("{\n");
+        s.push_str("[\n");
         for p in picked_points {
             let mut p_serialized = "{".to_string();
-            p_serialized.push_str(&format!("{{{}, {}}},", p.point.x, p.point.y)); // Pushes p.point
-            p_serialized.push_str(&format!("{},", p.id.to_string()));
+            p_serialized.push_str(&format!("\"point\":[{},{}],", p.point.x, p.point.y)); // Pushes p.point
+            p_serialized.push_str(&format!("\"id\":{},", p.id.to_string()));
             p_serialized.push_str(&format!(
-                "{{{}, {}, {}}},",
+                "\"opencv_color\":[{},{},{}],",
                 p.opencv_color.x, p.opencv_color.y, p.opencv_color.z
             ));
             p_serialized.push_str(&format!(
-                "{{{}, {}, {}}}",
+                "\"opengl_color\":[{}, {}, {}]",
                 p.opengl_color.x, p.opengl_color.y, p.opengl_color.z
             ));
             p_serialized.push_str("},\n");
             s.push_str(&p_serialized);
         }
-        s.push('}');
+        s.remove(s.len() - 2);
+        s.push(']');
         s
     }
 
     fn serialize_glm_vec3(v: &glm::Vec3) -> String {
-        let mut s = "{".to_string();
+        let mut s = "[".to_string();
         s.push_str(&format!("{},{},{}", v.x, v.y, v.z));
-        s.push('}');
+        s.push(']');
         s
     }
 }
